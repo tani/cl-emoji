9f0483c6d84650fa9638d2781e080c4f59a88b20
TANIGUCHI Masaya <mew@cat.ovh>
2015-08-17 16:49:42 +0900

    first commit

---
+++ README.markdown
@@ -0,0 +1,36 @@
+# :smile: Cl-Emoji
+cl-emoji provides the Unicode emoji characters
+
+:smile: :heart_eyes: :scream: :alien: :fire: :zzz: :hand:
+
+## :boom: Usage
+
+```lisp
+(ql:quicklaod :cl-emoji)
+(emoji:emoji :code "U+1F600")
+=> 😀
+(emoji:emoji :name "grinning face")
+=> 😀
+(emoji:annot :annotation "face")
+=> (("😀" "U+1F600" "grinning face" ("face" "grin" "person")) ...)
+```
+
+see also [Full Emoji Data](http://unicode.org/emoji/charts/full-emoji-list.html)
+
+## :fire: Installation
+
+```shell
+$ cd ~/common-lisp
+$ git clone git://github.com/ta2gch/cl-emoji
+```
+
+## :laughing: Author
+
+* Masaya TANIGUCHI (ta2gch@gmail.com)
+
+## :ok_hand: Copyright
+
+Copyright (c) 2015 Masaya TANIGUCHI (ta2gch@gmail.com)
+
+* src/cl-emoji.lisp is licensed under the MIT License
+* data/emoji-list.lisp is licensed under the Unicoded License
---
+++ src/cl-emoji.lisp
@@ -0,0 +1,45 @@
+#|
+Copyright (c) 2015 Masaya TANIGUCHI
+
+This software is released under the MIT license:
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+|#
+
+(cl:in-package #:cl-user)
+(defpackage #:cl-emoji
+  (:use #:cl)
+  (:nicknames #:emoji)
+  (:export emoji))
+(in-package #:cl-emoji)
+
+(defun emoji (&key (code nil code-supplied-p)
+		(name nil name-supplied-p)
+		(annotation nil annotation-supplied-p))
+  (with-open-file (s (asdf:system-relative-pathname :cl-emoji #p"data/emoji-list.lisp"))
+    (let ((emoji-list (read s)))
+      (cond
+	(code-supplied-p
+	 (find-if (lambda (c) (string= code (second c))) emoji-list))
+	(name-supplied-p
+	 (find-if (lambda (n) (string= name (third n))) emoji-list))
+	(annotation-supplied-p
+	 (loop for a in emoji-list
+	       if (member annotation (fourth a) :test #'string=)
+		 collect a))))))
