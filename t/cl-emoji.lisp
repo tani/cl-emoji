5d447fe49941fdcd3378a9c3f3efcd3d7b103d89
t-sin <shinichi.tanaka45@gmail.com>
2017-04-28 00:15:18 +0900

    List annots, groups and subgroups

--- t/cl-emoji.lisp
+++ t/cl-emoji.lisp
@@ -11,13 +11,16 @@
 		    :cl-emoji (pathname (format nil "data/emoji_~a.lisp"
                                         cl-emoji::*current-version*))))
   (let ((emoji-list (read s)))
-    (plan (+ 5 (length emoji-list)))
+    (plan (+ 8 (length emoji-list)))
     (dolist (u emoji-list)
       (is (length u) 6))
     (is "😀" (emoji:code '("U+1F600")))
     (is "😁" (emoji:name "grinning face with smiling eyes"))
     (ok (< 0 (length (emoji:annot "blue"))))
     (ok (< 0 (length (emoji:group "Smileys & People"))))
-    (ok (< 0 (length (emoji:subgroup "clothing"))))))
+    (ok (< 0 (length (emoji:subgroup "clothing"))))
+    (ok (< 0 (length (emoji:list-annots))))
+    (ok (< 0 (length (emoji:list-groups))))
+    (ok (< 0 (length (emoji:list-subgroups))))))
 
 (finalize)
